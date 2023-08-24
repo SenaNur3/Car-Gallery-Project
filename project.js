9a571fc6819582b07b3d6bb292b744a2b57c680f
Sena <se_na_e12@hotmail.com>
2023-08-24 17:29:43 +0300

    addCar

---
+++ project.js
@@ -0,0 +1,32 @@
+const form = document.getElementById("car-form");
+const titleElement = document.querySelector("#title");
+const priceElement = document.querySelector("#price");
+const urlElement = document.querySelector("#url");
+
+//Tüm Eventleri yükleme 
+
+eventListeners();
+
+function eventListeners(){
+   form.addEventListener("submit",addCar);
+}
+
+function addCar(e) {
+   e.preventDefault();
+
+   const title = titleElement.value;
+   const price = priceElement.value;
+   const url = urlElement.value;
+
+   if (title === "" || price === "" || url === "") {
+       ui.displayMessages("Tüm alanları doldurun..","danger");
+   } else {
+       const newCar = new Car(title, price, url);
+       ui.addCarToUI(newCar); // arayüze araç ekleme
+
+       storage.addCarToStorage(newCar);
+
+       ui.displayMessages("Araç başarıyla eklendi.","success");
+   }
+   ui.clearInputs(titleElement, priceElement, urlElement);
+}
